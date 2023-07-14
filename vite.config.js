ccea7d65a21ed167200b72dc97534b6acddb20d4
wangyi <921723561@qq.com>
2023-07-14 09:48:34 +0800

    代码提交

--- vite.config.js
+++ vite.config.js
@@ -71,11 +71,11 @@ export default defineConfig(({ mode }) => {
     css: {
       postcss: {
         plugins: [
-          // postCssPxToRem({
-          //   rootValue: 192, // 设计稿宽度的1/ 10 例如设计稿按照 1920设计 此处就为192
-          //   propList: ["*", "!border"], // 除 border 外所有px 转 rem
-          //   selectorBlackList: [".el-"], // 过滤掉.el-开头的class，不进行rem转换
-          // }),
+          postCssPxToRem({
+            rootValue: 192, // 设计稿宽度的1/ 10 例如设计稿按照 1920设计 此处就为192
+            propList: ["*", "!border"], // 除 border 外所有px 转 rem
+            selectorBlackList: [".el-"], // 过滤掉.el-开头的class，不进行rem转换
+          }),
           autoprefixer({
             overrideBrowserslist: [
               "Android 4.1",
